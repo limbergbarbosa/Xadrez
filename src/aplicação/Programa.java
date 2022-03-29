3ea478f1781ca73239572929e9b4a4af95d313cb
Limberg Barbosa <limbergbarbosa@gmail.com>
2022-03-28 21:11:49 -0300

    Primeiro commit

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,15 @@
+package aplicação;
+
+import tabuleiro.Posição;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posição p = new Posição(3,5);
+		
+		System.out.println(p);
+	}
+	
+  
+}
